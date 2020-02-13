08ac534f6712b5c64a26f5e7a20ac6ff62f2b3a8
sasaky <athene609.cyl@gmail.com>
2020-02-14 04:31:44 +0900

    :hammer: 空白トリムの挙動変更
    各行末のトリムもついでにやる

--- src/mod.ts
+++ src/mod.ts
@@ -72,7 +72,7 @@ String.prototype['pixivNovel2AozoraTxt'] = function() {
 		// HTML 除去
 		.replace(/<\/?div.*?>/gis, '')
 		.replace(/<\/h\d.*?>?/gi, '')
-		.replace(/<br \/>/gi, '\r\n')
+		.replace(/<br \/>/gi, '')
 		// 改ページ
 		.replace(/\[newpage\]/gi, '［＃改ページ］')
 		// チャプター
@@ -91,8 +91,15 @@ String.prototype['pixivNovel2AozoraTxt'] = function() {
 		.replace(/［＃横組み終わり］［＃横組み］/g, '')
 		.replace(/^［＃横組み］(.*)［＃横組み終わり］$/gm, '［＃ここから横組み］\r\n$1\r\n［＃ここで横組み終わり］')
 		.replace(/^［＃ここで横組み終わり］\r?\n［＃ここから横組み］\r?\n/gm, '')
-		// 添え物のような端トリム
-		.trim()
+		// jumpurl
+		.replace(/\[\[jumpuri:(.*?) ?(?:&gt;|>) ?(.*?)\]\]/, '$1 <$2>')
+		// 各行の行末空白をトリム
+		.split(/\r?\n/)
+		.map(s => s.trimRight())
+		.join('\r\n')
+		// 文章先頭は全角スペース以外トリム、終端は通常トリム＋空行挿入
+		.replace(/^[\f\n\r\t\v \u00a0\u1680​\u180e\u2028\u2029\u202f\u205f​\ufeff\u{2000}-\u{200a}​]+/u, '')
+		.trimRight() + '\r\n'
 }
 const virtualConsole = new jsdom.VirtualConsole()
 
