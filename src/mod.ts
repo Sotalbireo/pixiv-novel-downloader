e6bfc7a6a0c095ae54e30a5fe346164d6c3d788a
sasaky <athene609.cyl@gmail.com>
2020-02-13 17:08:12 +0900

    :hammer:

--- src/mod.ts
+++ src/mod.ts
@@ -3,15 +3,21 @@ import { homedir } from 'os'
 import jsdom from 'jsdom'
 const { JSDOM } = jsdom
 
+declare global {
+	interface String {
+		pixivNovel2AozoraTxt(): string
+	}
+}
+
 if (process.argv.length < 3) {
 	console.log(process.argv)
 	process.exit()
 }
 
 
-const items = [...process.argv].filter(arg => /^https?:\/\//.test(arg))
+const items = [...process.argv].slice(2)
 const baseDir = `${homedir()}/Downloads/pixiv-novel`
-const fixDirectoryLikeString = (str: string) => {
+const stylizeLikePath = (str: string) => {
 	const uselessChar = {
 		'\/': '／',
 		'\>': '＞',
@@ -28,9 +34,10 @@ const fixDirectoryLikeString = (str: string) => {
 	const re = RegExp(`[${Object.keys(uselessChar).join('')}]`, 'gi')
 	return str.replace(re, (m: string) => uselessChar[m as keyof typeof uselessChar])
 }
-const pixivNovel2AozoraTxt = (str: string) => {
-	return str
+String.prototype['pixivNovel2AozoraTxt'] = function() {
+	return this
 		// HTML 除去
+		.replace(/<\/?div.*?>/gis, '')
 		.replace(/<\/h\d.*?>?/gi, '')
 		.replace(/<br \/>/gi, '\r\n')
 		// 改ページ
@@ -59,7 +66,7 @@ if (!fs.existsSync(baseDir)) {
 const virtualConsole = new jsdom.VirtualConsole()
 
 ;(async () => {
-	for await (const item of items) {
+	for await (let item of items) {
 
 		if (!/^https?:\/\/www\.pixiv\.net\/novel\/show.php/.test(item)) {
 			continue
@@ -81,15 +88,15 @@ const virtualConsole = new jsdom.VirtualConsole()
 
 		const dom = raw!.window.document as Document
 		const userdata = dom.querySelectorAll('.userdata')[0]
-		const title = fixDirectoryLikeString(userdata.querySelector('.title')?.textContent!)
-		const author = fixDirectoryLikeString(userdata.querySelector('.name a')?.textContent!)
+		const title = stylizeLikePath(userdata.querySelector('.title')?.textContent!)
+		const author = stylizeLikePath(userdata.querySelector('.name a')?.textContent!)
 		const authorId = (userdata.querySelector('.name a')?.getAttribute('href')?.match(/\/(\d+)$/) as any[])[1] as number
 		const authorDir = `${authorId}_${author}`
 		const articleId = (new URL(item).search.match(/\&?id=(\d+)/) as any[])[1] as number
 		const fileName = `${articleId}_${title}`
-		const article = dom.querySelector('.novelbody-container noscript')?.textContent!
+		const article = dom.querySelector('.novelbody-container noscript')?.textContent!.pixivNovel2AozoraTxt()
 
-		const aozoraTxt = [title, author, item, '\r\n\r\n', pixivNovel2AozoraTxt(article)].join('\r\n')
+		const aozoraTxt = [title, author, item, '\r\n\r\n', article].join('\r\n')
 
 		if (!fs.existsSync(`${baseDir}/${authorDir}`)) {
 			fs.mkdirSync(`${baseDir}/${authorDir}`, { recursive: true })
