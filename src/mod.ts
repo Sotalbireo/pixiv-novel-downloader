17a5f65e2c4a253c0caa9fd6653cd3b7f04ae78c
sasaky <athene609.cyl@gmail.com>
2020-02-14 18:34:04 +0900

    :hammer:

--- src/mod.ts
+++ src/mod.ts
@@ -27,10 +27,12 @@ const resolveArgv = (argv: string[]): string[] => {
 			idx = argv.indexOf(option, idx + 1)
 		}
 		indices.forEach(i => {
-			const file = resolve(argv[i + 1])
 			try {
-				const importArgv = fs.readFileSync(file, 'utf-8')
-					.split(/\r?\n/).map(s => s.trim())
+				const file = resolve(argv[i + 1])
+				const importArgv = fs
+					.readFileSync(file, 'utf-8')
+					.split(/\r?\n/)
+					.map(s => s.trim())
 				argv.splice(i, 2)
 				argv.push(...importArgv)
 			} catch (err) {
@@ -134,16 +136,16 @@ if (!fs.existsSync(baseDir)) {
 		}
 
 		const dom = raw.window.document
-		const userdata = dom.querySelectorAll('.userdata')[0]
-		const title = stylizeLikePath(userdata.querySelector('.title')?.textContent!)
-		const author = stylizeLikePath(userdata.querySelector('.name a')?.textContent!)
-		const authorId = (userdata.querySelector('.name a')?.getAttribute('href')?.match(/\/(\d+)$/) as any[])[1] as number
+		const userdata = dom.querySelector('.userdata')!
+		const title = stylizeLikePath(userdata.querySelector('.title')?.textContent ?? '')
+		const articleId = new URL(item).search.match(/\&?id=(\d+)/)![1]
+		const author = stylizeLikePath(userdata.querySelector('.name a')?.textContent ?? '')
+		const authorId = userdata.querySelector('.name a')?.getAttribute('href')?.match(/\/(\d+)$/)![1]
 		const authorDir = `${authorId}_${author}`
-		const articleId = (new URL(item).search.match(/\&?id=(\d+)/) as any[])[1] as number
 		const fileName = `${articleId}_${title}`
-		const article = dom.querySelector('.novelbody-container noscript')?.textContent!.pixivNovel2AozoraTxt()
+		const article = dom.querySelector('.novelbody-container noscript')?.textContent?.pixivNovel2AozoraTxt() ?? ''
 
-		const aozoraTxt = [title, author, item, '\r\n\r\n', article].join('\r\n')
+		const aozoraTxt = [title, author, item, '', '', article].join('\r\n')
 
 		if (!fs.existsSync(`${baseDir}/${authorDir}`)) {
 			fs.mkdirSync(`${baseDir}/${authorDir}`, { recursive: true })
